fc108b14912d0b8a00e8d99b8d3d9ba7c6c0aad4
Morgan Smith <morgan.smith@crosskey.fi>
2024-05-07 20:22:51 +0300

    Implemented attached images to post view

--- pages/PostPage.tsx
+++ pages/PostPage.tsx
@@ -1,10 +1,25 @@
 import { PostView } from "lemmy-js-client";
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageURISource } from "react-native";
 import { CommonStyles } from "../styles/Global";
 import { PostStyles } from "../styles/Post";
+import FullWidthImage from 'react-native-fullwidth-image'
 
 export function PostPage({ route, navigation }) {
 	let { post }: { post: PostView } = route.params;
+
+	function displayImage() {
+		console.log(post.post);
+		if (post.post.url != undefined) {
+			return (
+				<View style={{overflow: "hidden"}}>
+					<FullWidthImage
+						source={{ uri: post.post.url }}
+						style={{ width:"100%"}}
+					/>
+				</View>
+			);
+		}
+	}
 
 	return (
 		<View style={[CommonStyles.page]}>
@@ -15,6 +30,7 @@ export function PostPage({ route, navigation }) {
 				<Text style={[PostStyles.title]}>{post.post.name}</Text>
 			</View>
 			<Text style={[PostStyles.body]}>{post.post.body}</Text>
+			{displayImage()}
 		</View>
 	);
 }
